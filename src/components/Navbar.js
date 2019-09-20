a9fb6bcc15aec8ed13e38196b95ec462dee6b529
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2019-09-20 17:46:38 +0530

    further navbar testing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,9 +30,9 @@ class Navbar extends Component {
 							<li className="nav-item">
 								<Link className="nav-link" to={'/gonzo'}> Gonzo Soccer </Link>
 							</li>
-							<li className="nav-item">
-								<Link className="nav-link" to={'/sponsors'}> Sponsors </Link>
-							</li>
+							// <li className="nav-item">
+							// 	<Link className="nav-link" to={'/sponsors'}> Sponsors </Link>
+							// </li>
 						</ul>
 					</div>
 				</div>
