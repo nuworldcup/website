f91931eb11e9afb1da439a95b85eb8f7090c1ebf
Angel Croce <17angelc@gmail.com>
2019-10-10 17:49:54 -0500

    Changed caption and added link to Facebook Event

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from '../assets/img/nuwclogo.jpg';
 // import logomx from '../assets/img/nuwcmxwhite.png';
 // import wickedlogo from '../assets/img/wicked-cup/wickedcupbackground.jpg';
 // import winterfs from '../assets/img/futsal-tourney/nuwcwinterfutsal.png';
-import nigeria from '../assets/img/nuwclogo.jpg';
+import logo from "../assets/img/nuwclogo.jpg";
 
 class Homepage extends Component {
-    render() {
-		return (
-			<div> 
-				<div className="d-flex justify-content-center home-logo-div">
-					<img className="home-logo" src={nigeria} alt={"Northwestern World Cup 2019"}/>
-				</div>
-				<div className="d-flex justify-content-center">
-					<p className="lead">Community. Philanthropy. Soccer. </p>
-				</div>
-
-
-			</div>
-			);
-	}
+  render() {
+    return (
+      <div>
+        <div>
+          <a
+            href="https://www.facebook.com/events/595818960951941"
+            className="d-flex justify-content-center home-logo-div"
+          >
+            <img
+              className="home-logo"
+              src={logo}
+              alt={"Northwestern World Cup 2019"}
+            />
+          </a>
+        </div>
+        <div className="d-flex justify-content-center">
+          <p className="lead">
+            {" "}
+            Join us in our support of NU Women's Soccer this Friday! Click on
+            our logo for the Facebook Event.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
