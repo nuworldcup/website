34fa93b5b19e63045291674392f7572313cbbdfc
Angel Croce <17angelc@gmail.com>
2020-01-24 11:09:53 -0600

    Site ready for Futsal Reg

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,8 +4,9 @@ import React, { Component } from 'react'
 // import wickedlogo from '../assets/img/wicked-cup/wickedcupbackground.jpg';
 // import winterfs from '../assets/img/futsal-tourney/nuwcwinterfutsal.png';
 import Footer from './Footer'
-import logo from '../assets/img/wicked-cup/wickedcuplogo.png'
+import logo from '../assets/img/nuwclogo.jpg'
 import fifa from '../assets/img/Fifa-Tourney-Banner.jpg'
+import futsal from '../assets/img/futsal-tourney/nuwcwinterfutsal.png'
 
 class Homepage extends Component {
   render() {
@@ -15,15 +16,23 @@ class Homepage extends Component {
         <div className='d-flex justify-content-center home-logo-div'>
           <img
             class='home-logo'
-            src={logo}
+            src={futsal}
             alt={'Northwestern World Cup 2019'}
           />
         </div>
         <div className='d-flex justify-content-center'>
           <p className='lead'> Community. Philanthropy. Soccer.</p>
         </div>
+        <div>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScrgCt121Wujo4PNwBdMKI5ZFvzJQ0Y3lUEjkvRJe_X4GO5vw/viewform?embedded=true" width="640" height="1080" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
+        </div>
+        <div>
+
+
+        </div>
         <Footer />
-      </div>
+      </div >
 
     )
   }
