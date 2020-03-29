ec3b89d64d434cfaf4383dd0f992875488343271
Angel Croce <17angelc@gmail.com>
2020-03-29 18:27:40 -0500

    fixed resizing for text on new homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,11 +6,11 @@ import React, { Component } from 'react'
 import Footer from './Footer'
 import logo from '../assets/img/nuwclogo.jpg'
 import nigeria from '../assets/img/nigerialogo.png'
-import futsal from '../assets/img/futsal-tourney/nuwcwinterfutsal.png'
+import futsal from '../assets/img/futsal-tourney/nuwcwinterfutsal-square.png'
 import mexico from '../assets/img/nuwcmxalt.png'
-import pic1 from '../assets/img/gallery/pic1.jpg'
-import pic2 from '../assets/img/gallery/pic2.jpg'
-import pic3 from '../assets/img/gallery/pic3.jpg'
+import pic17 from '../assets/img/gallery/pic17.JPG'
+import pic18 from '../assets/img/gallery/pic18.JPG'
+import pic19 from '../assets/img/gallery/pic19.jpg'
 import gonzo from '../assets/img/gonzo/gonzo.jpg'
 
 class Homepage extends Component {
@@ -23,20 +23,29 @@ class Homepage extends Component {
             <img class='home-logo center'
               src={logo}>
             </img>
+
+
+            <h1 class='header-text center'>
+              Community. Philanthropy. Soccer.
+            </h1>
+
           </div>
 
           <div class='homepage-container'>
+            <div class='grid-cell-25'>
+              <h2 class='header-text'>About Us</h2>
+            </div>
 
             <div class='grid-cell-2'>
               <div id='carouselcontrols' class='carousel slide center' data-ride='carousel'>
                 <div class='carousel-inner'>
                   <div class='carousel-item active'>
-                    <img class='d-block' src={pic1} alt='First slide' /></div>
+                    <img class='d-block landing-gallery' src={pic17} alt='First slide' /></div>
                   <div class='carousel-item'>
-                    <img class='d-block ' src={pic2} alt='Second slide' />
+                    <img class='d-block landing-gallery' src={pic18} alt='Second slide' />
                   </div>
                   <div class='carousel-item'>
-                    <img class='d-block' src={pic3} alt='Third slide' />
+                    <img class='d-block landing-gallery' src={pic19} alt='Third slide' />
                   </div>
                 </div>
                 <a class='carousel-control-prev' href='#carouselcontrols' role='button' data-slide='prev'>
@@ -50,13 +59,6 @@ class Homepage extends Component {
               </div>
             </div>
 
-            <div class='grid-cell-25'>
-              <h1>About Us</h1>
-            </div>
-
-            <div class='grid-cell-35'>
-              <h1>Past Events</h1>
-            </div>
 
             <div class='grid-cell-3'>
               <div id='carouselcontrol' class='carousel slide center' data-ride='carousel'>
@@ -81,9 +83,13 @@ class Homepage extends Component {
               </div>
             </div>
 
+            <div class='grid-cell-35'>
+              <h2 class='header-text'>Past Events</h2>
+            </div>
+
             <div class='grid-cell-1'>
-              <h1 class='home-content'>What is Gonzo?</h1>
-              <p>A non-profit that uses soccer to empower girls in order to achieve their potential</p>
+              <h2 class='home-content header-text'>What is Gonzo?</h2>
+              <p class='info-text'>A non-profit that uses soccer to empower girls in order to achieve their potential</p>
             </div>
             <div class='grid-cell-1'>
               <img src={gonzo}></img>
