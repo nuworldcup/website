76c797689b169054abf641e0822a3c83692a8dd6
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-04-17 14:30:02 -0500

    responsive, jquery button, spacing issues resolved

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -16,9 +16,11 @@ import monster from '../assets/img/sponsors/2016/monster.jpg'
 import redstars from '../assets/img/sponsors/2019/redstars.png'
 import president from '../assets/img/sponsors/2018/president.png'
 import loop from '../assets/img/sponsors/2018/loop.png'
+import { Link as Animationlink } from 'react-scroll';
 
 
 import fire from '../assets/img/sponsors/old/chicagofire.png'
+import downarrow from '../assets/img/downarrow.png'
 
 import '../homepage.css'
 
@@ -27,82 +29,91 @@ class Homepage extends Component {
   render() {
     return (
       <div>
-        <div class="topsection">
-          <section>
-            <img class='main-logo-div center' src={logo}></img>
-          </section>
-          <section>
-            <h1 class='logo-text'> Community. Philanthropy. Soccer.</h1>
-          </section>
+        <div className='d-flex justify-content-center home-logo-div'>
+          <img
+            className='home-logo'
+            src={logo}
+            alt={'Northwestern World Cup 2019'}/>
         </div>
-        <div class="page-wrap panel2" id="panel2">
-          <section class="main-content" role="main">
-            <p class="lead homepagetext">We run a variety of events every year, including tournaments
+        <div className='d-flex justify-content-center'>
+          <p className='lead'> Community. Philanthropy. Soccer.</p>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <p className='lead'>
+            <Animationlink to="scrolltarget" smooth={true}  duration= {500}>
+              <img className="downarrow" src={downarrow} alt="Down arrow"/>
+            </Animationlink>
+          </p>
+        </div>
+        <div className="flex-container panel2" name="scrolltarget">
+          <div className="container">
+            <p className="lead homepagetext" >We run a variety of events every year, including tournaments
               (futsal, FIFA, halloween-themed), cultural events, freestyler shows, and trips to Chicago Fire games. </p>
-          </section>
-          <section class="main-content" role="complementary">
-            <div id="carouselExampleControls" class="carousel slide carousel1-images carousel-center" data-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="w-100" src={nigeria} alt="First slide" />
+          </div>
+          <div className="container">
+            <div id="carouselExampleControls" className="carousel slide carousel-center" data-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="w-100 carouselimg" src={nigeria} alt="First slide" />
                 </div>
-                <div class="carousel-item">
-                  <img class="w-100" src={futsal} alt="Second slide" />
+                <div className="carousel-item">
+                  <img className="w-100 carouselimg" src={futsal} alt="Second slide" />
                 </div>
-                <div class="carousel-item">
-                  <img class="w-100" src={mexico} alt="Third slide" />
+                <div className="carousel-item">
+                  <img className="w-100 carouselimg" src={mexico} alt="Third slide" />
                 </div>
-                <div class="carousel-item">
-                  <img class="w-100" src={korjap} alt="Fourth slide" />
+                <div className="carousel-item">
+                  <img className="w-100 carouselimg" src={korjap} alt="Fourth slide" />
                 </div>
               </div>
-              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+              <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
               </a>
-              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+              <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
               </a>
             </div>
-          </section>
+          </div>
         </div>
-        <div class="page-wrap panel3">
-          <section class="main-content" role="main">
-            <img class='home-logo center' src={gonzo}></img>
-          </section>
-          <section class="main-content" role="complementary">
-            <p class="lead homepagetext">We're also very proud of our affiliation
+        <div className="flex-container">
+          <div className="container">
+            <img className='home-logo center gonzoimage' src={gonzo}></img>
+          </div>
+          <div className="container">
+            <p className="lead homepagetext">We're also very proud of our affiliation
             with Gonzo Soccer: a Chicago non profit that empowers young girls through the beautiful game. World Cup partners
-                with Gonzo to provide the girls academic mentorship. Check them out <a href="https://www.facebook.com/GONZOsoccer/">here</a>!
-              </p>
-          </section>
+            with Gonzo to provide the girls academic mentorship. Check them out <a href="https://www.facebook.com/GONZOsoccer/">here</a>!
+            </p>
+          </div>
         </div>
-        <div class="page-wrap panel4">
-          <section class="main-content" role="main">
-            <p class="lead homepagetext">World Cup recruits freshmen, sophomores, juniors, and seniors
+        <div className="flex-container panel4">
+          <div className="container containerhomepg">
+            <p className="lead homepagetext">World Cup recruits freshmen, sophomores, juniors, and seniors
             every year in the fall. To get in touch, email us at northwesternworldcup@gmail.com,
             or reach out to one of our exec board through the contact tab!
-              </p>
-          </section>
-          <section class="main-content" role="complementary">
-            <img class='home-logo center' src={grouppic}></img>
-          </section>
+            </p>
+          </div>
+          <div className="container containerhomepg">
+            <img className='home-logo center gonzoimage' src={grouppic}></img>
+          </div>
         </div>
-        <div class="page-wrap panel1">
-            <section class="main-content" role="main">
-              <img class="sponsorimg" src={subway}></img>
-              <img class="sponsorimg" src={fire}></img>
-              <img class="sponsorimg" src={kind}></img>
-              <img class="sponsorimg" src={monster}></img>
-              <img class="sponsorimg" src={redstars}></img>
-              <img class="sponsorimg" src={loop}></img>
-              <img class="sponsorbottomimg" src={president}></img>
-            </section>
-            <section class="main-content" role="complementary">
-              <p class="lead homepagetext">We're blessed to have partnered with a number of great companies in the last few years. World Cup
-                wouldn't be the same without our sponsors. </p>
-            </section>
+        <div className="flex-container panel1">
+          <div className="container">
+              <img className="sponsorimg" src={subway}></img>
+              <img className="sponsorimg" src={fire}></img>
+              <img className="sponsorimg" src={kind}></img>
+              <img className="sponsorimg" src={monster}></img>
+              <img className="sponsorimg" src={redstars}></img>
+              <img className="sponsorimg" src={loop}></img>
+              <img className="sponsorbottomimg" src={president}></img>
+          </div>
+          <div className="container">
+            <p className="lead homepagetext">We're blessed to have partnered with a number of great companies in the last few years. World Cup
+              wouldn't be the same without our sponsors. 
+            </p>
+          </div>
         </div>
       </div>
     )
