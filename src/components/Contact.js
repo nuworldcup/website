72df6715bfa0953123d9825124516986cb687834
kevinkim12 <67163421+kevinkim12@users.noreply.github.com>
2020-09-18 21:30:01 -0500

    contact page pictures

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import grouppic from '../assets/img/grouppic.jpeg';
 import '../contact.css'
+import prez from '../assets/img/vikram.jpg'
 
 class Contact extends Component {
 	render() {
@@ -11,21 +12,21 @@ class Contact extends Component {
 					<div class='page-wrap'>
 						<section class='contact-content'>
 							<h1>President</h1>
-							<img scr='...' alt='placeholder' />
-							<p>Vikram Manikantan</p>
-							<p><small>VikramManikantan2022@u.northwestern.edu</small></p>
+							<img src={prez} alt='placeholder' />
+							<p>Vikram Manikantan
+							<small> VikramManikantan2022@u.northwestern.edu</small></p>
 						</section>
 						<section class='contact-content'>
 							<h1>Vice President</h1>
 							<img src='...' alt='placeholder' />
-							<p>Alvaro Matos</p>
-							<p><small>alvaromatos2021@u.northwestern.edu</small></p>
+							<p>Alvaro Matos
+							<small> alvaromatos2021@u.northwestern.edu</small></p>
 						</section>
 						<section class='contact-content'>
 							<h1>Treasurer</h1>
 							<img src='...' alt='placeholder' />
-							<p>Brandon Kyung</p>
-							<p><small>brandonkyung2022@u.northwestern.edu</small></p>
+							<p>Brandon Kyung
+							<small> brandonkyung2022@u.northwestern.edu</small></p>
 						</section>
 						<br /> <br />
 					</div>
