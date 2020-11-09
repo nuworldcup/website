c7a41cccb0bdf87364a18231f8dd65569c2ad496
kevinkim12 <67163421+kevinkim12@users.noreply.github.com>
2020-11-08 21:54:13 -0600

    registration

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -4,7 +4,7 @@ import wickedlogo from '../assets/img/wicked-cup/wickedcuplogosmall.png'
 
 class Registration extends React.Component{
   render() {
-  return(
+    return(
     // <html>
     //   <body>
     //       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfP6wD0laJupTyQiQvylZtJDmQiTbCCp62alYO0m5Q7oK_rxg/viewform?embedded=true"
@@ -15,20 +15,20 @@ class Registration extends React.Component{
     //       </iframe>
     //   </body>
     // </html>
-    <div>
-      <div className='d-flex justify-content-center'>
-        <img className='wicked-logo'
-              src={wickedlogo}
-              alt={'Wicked Cup 2020'} />
-      </div>
       <div>
-        <br></br><br></br>
-        <h1 className='d-flex justify-content-center'>
-          Registration coming soon!
-        </h1>
-        <br></br><br></br><br></br>
+        <div className='d-flex justify-content-center'>
+          <img className='wicked-logo'
+                src={wickedlogo}
+                alt={'Wicked Cup 2020'} />
+        </div>
+        <div>
+          <br></br><br></br>
+          <h1 className='d-flex justify-content-center'>
+            Registration coming soon!
+          </h1>
+          <br></br><br></br><br></br>
+        </div>
       </div>
-    </div>
   )
   }
 }
