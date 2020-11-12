35b4f99ef22895b5dd662a851eb576e1f06c1ed2
kevinkim12 <67163421+kevinkim12@users.noreply.github.com>
2020-11-11 21:16:33 -0600

    registration e Please enter the commit message for your changes. Lines starting

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -5,30 +5,30 @@ import wickedlogo from '../assets/img/wicked-cup/wickedcuplogosmall.png'
 class Registration extends Component{
   render() {
     return(
-    // <html>
-    //   <body>
-    //       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfP6wD0laJupTyQiQvylZtJDmQiTbCCp62alYO0m5Q7oK_rxg/viewform?embedded=true"
-    //         width="640" height="861"
-    //         frameborder="0"
-    //         marginheight="0" marginwidth="0">
-    //         Loading…
-    //       </iframe>
-    //   </body>
-    // </html>
-      <div>
-        <div className='d-flex justify-content-center'>
-          <img className='wicked-logo'
-                src={wickedlogo}
-                alt={'Wicked Cup 2020'} />
-        </div>
-        <div>
-          <br></br><br></br>
-          <h1 className='d-flex justify-content-center'>
-            Registration coming soon!
-          </h1>
-          <br></br><br></br><br></br>
-        </div>
-      </div>
+    <html>
+      <body>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRv1qAcHzduACHvMqvln2AkGjoi5BiJLcuO0vrzexWpwP1iQ/viewform?embedded=true"
+            width="640" height="2903"
+            frameborder="0"
+            marginheight="0" marginwidth="0">
+            Loading…
+          </iframe>
+      </body>
+    </html>
+      // <div>
+      //   <div className='d-flex justify-content-center'>
+      //     <img className='wicked-logo'
+      //           src={wickedlogo}
+      //           alt={'Wicked Cup 2020'} />
+      //   </div>
+      //   <div>
+      //     <br></br><br></br>
+      //     <h1 className='d-flex justify-content-center'>
+      //       Registration coming soon!
+      //     </h1>
+      //     <br></br><br></br><br></br>
+      //   </div>
+      // </div>
   );
   }
 }
