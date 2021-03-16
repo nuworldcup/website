f79a128e4f99c821b2bfab0f20d6f1f654cdb909
kevinkim12 <67163421+kevinkim12@users.noreply.github.com>
2021-03-16 15:55:26 -0500

    social media icons

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -5,30 +5,29 @@ import wickedlogo from '../assets/img/wicked-cup/wickedcuplogosmall.png'
 class Registration extends Component{
   render() {
     return(
-    <html>
-      <body>
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRv1qAcHzduACHvMqvln2AkGjoi5BiJLcuO0vrzexWpwP1iQ/viewform?embedded=true"
-            width="640" height="2903"
-            frameborder="0"
-            marginheight="0" marginwidth="0">
-            Loading…
-          </iframe>
-      </body>
-    </html>
+    // <html>
+    //   <body>
+    //       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRv1qAcHzduACHvMqvln2AkGjoi5BiJLcuO0vrzexWpwP1iQ/viewform?embedded=true"
+    //         width="640" height="2903"
+    //         frameborder="0"
+    //         marginheight="0" marginwidth="0">
+    //         Loading…
+    //       </iframe>
+    //   </body>
+    // </html>
       // <div>
       //   <div className='d-flex justify-content-center'>
-      //     <img className='wicked-logo'
-      //           src={wickedlogo}
-      //           alt={'Wicked Cup 2020'} />
+      //     // <img className='wicked-logo'
+      //     //       src={wickedlogo}
+      //     //       alt={'Wicked Cup 2020'} />
       //   </div>
-      //   <div>
-      //     <br></br><br></br>
-      //     <h1 className='d-flex justify-content-center'>
-      //       Registration coming soon!
-      //     </h1>
-      //     <br></br><br></br><br></br>
-      //   </div>
-      // </div>
+        <div>
+          <br></br><br></br>
+          <h1 className='d-flex justify-content-center'>
+            Registration coming soon!
+          </h1>
+          <br></br><br></br><br></br>
+        </div>
   );
   }
 }
